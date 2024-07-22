207eefc050de12510393019135f97aec297acf92
rishikvissa7 <rishikvissa7@gmail.com>
2024-07-22 18:14:27 +0530

    learned crud operations

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const mongoose = require('mongoose');
+const Product = require('./models/product.model.js')
+const app = express()
+
+app.use(express.json());
+
+
+app.get('/',(req,res) => {
+    res.send("hello from node API Server Updated");
+});
+
+app.get('/api/products', async(req,res) => {
+    try {
+        const products = await Product.find({});
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+})
+
+app.get('/api/product/:id', async(req,res) => {
+    try {
+        const { id } = req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product)
+        
+    } catch (error) {
+        res.status(500).json({message: error.message});
+        
+    }
+})
+
+app.post('/api/products', async(req,res) => {
+    try {
+        const product = await Product.create(req.body);
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+})
+
+mongoose.connect("mongodb+srv://rishikvissa7:admin123@backenddb.i3jxebn.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB")
+.then(() => {
+    console.log("Connected to database!");
+    app.listen(3000, () => {
+        console.log('Server is running on port 3000');
+    });
+
+})
+.catch(() => {
+  console.log("Connection failed");  
+})
